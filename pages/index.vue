05e7f872a4c7ce227cb8a7104c26d92182173dfd
Ekky Sukmawati <ekkys99@gmail.com>
2023-10-17 09:50:39 +0700

    Add Edit button : action memanggil di field

--- pages/index.vue
+++ pages/index.vue
@@ -7,12 +7,16 @@
           <b-card class="shadow-md border-o rounded-lg">
             <h5>DATA POSTS</h5>
             <hr>
-            <b-button :to="{ name: 'post-create' }" variant="danger" class="mb-3">
+            <b-button :to="{ name: 'post-create' }" variant="info" class="mb-3">
               TAMBAH
             </b-button>
             <b-table striped bordered hover :items="posts" :fields="fields" show-empty>
-
+              <template v-slot:cell(action)="row">
+                <b-button :to="{ name: 'post-edit-id', params: { id: row.item.id } }" variant="warning"
+                  size="sm">EDIT</b-button>
+              </template>
             </b-table>
+
           </b-card>
         </b-col>
       </b-row>
